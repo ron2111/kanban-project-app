47c15237b8d986b4cf42c7466d346c000f546519
Rohan Sharma <onlyrohan21@gmail.com>
2023-11-27 22:24:47 +0530

    list-implement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const statusList = ["In progress", "Backlog", "Todo", "Done", "Cancelled"];
+  const userList = [
+    "Anoop sharma",
+    "Yogesh",
+    "Shankar Kumar",
+    "Ramesh",
+    "Suresh",
+  ];
+  const priorityList = [
+    { name: "No priority", priority: 0 },
+    { name: "Low", priority: 1 },
+    { name: "Medium", priority: 2 },
+    { name: "High", priority: 3 },
+    { name: "Urgent", priority: 4 },
+  ];
+
+  function handleGroupValue(value) {
+    setgroupValue(value);
+    console.log(value);
+  }
+
+  function handleOrderValue(value) {
+    setorderValue(value);
+    console.log(value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
